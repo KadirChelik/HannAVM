6a4be8af4e6a7d2b8b7c21aa31786829a95f2c2d
Kadir Celik <kdrclk7209lk@gmail.com>
2024-05-17 21:38:18 +0300

    baseurl

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: "http://localhost:5173", // your server base URL
+  baseURL: "http://hann-avm-frontend.vercel.app", // your server base URL
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json;charset=UTF-8"
